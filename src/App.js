364f3dea86462eea78f40ff650f916604d68af44
Emon Li <emonli.328@gmail.com>
2023-01-14 16:55:15 -0500

    imported NewResumeForm components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import NewResumeForm from "./components/NewResumeForm"
-import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import NewResumeForm from "./components/NewResumeForm";
+import "./App.css";
 
 const App = () => {
   // HOOKS
@@ -13,11 +14,13 @@ const App = () => {
   // }, []);
 
   return (
-  <>
-    <div>
-      <h1>Resume Swyper</h1>
-    </div>
-  </>
-)}
+    <>
+      <div>
+        <NewResumeForm />
+        {/* <h1>Resume Swyper</h1> */}
+      </div>
+    </>
+  );
+};
 
 export default App;
