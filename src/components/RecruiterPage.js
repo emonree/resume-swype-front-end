b932adc812486ad19d4b70ba3f67528497562412
Emon Li <emonli.328@gmail.com>
2023-01-15 21:30:00 -0500

    added content to recruiterpage.js

--- src/components/RecruiterPage.js
+++ src/components/RecruiterPage.js
@@ -1,35 +1,41 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import { useSelector, useDispatch } from 'react-redux'
-import { setResumes } from '../features/resume/resumeSlice'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useSelector, useDispatch } from "react-redux";
+import { setResumes } from "../features/resume/resumeSlice";
+import TinderCard from "react-tinder-card";
 import "./RecruiterPage.css";
 
 const RecruiterPage = () => {
-
-  const resumes = useSelector((state) => state.resume.resumes)
-  const dispatch = useDispatch()
+  const resumes = useSelector((state) => state.resume.resumes);
+  const dispatch = useDispatch();
   // const [resumes, setResumes] = useState([])
   // on page load, get the resume data with axios
 
   // useEffect goes here
   useEffect(() => {
-    console.log("......")
-    axios.get("http://localhost:8000/api/resumes")
-      .then((response) => {
-        dispatch(setResumes(response.data)); // updates the redux store with the resumes data
+    console.log("......");
+    axios.get("http://localhost:8000/api/resumes").then((response) => {
+      dispatch(setResumes(response.data)); // updates the redux store with the resumes data
     });
-  }, [])
+  }, []);
 
-  console.log(resumes)
+  console.log(resumes);
 
   // const handleAccept = () => {
 
   // }
+
   return (
     <div>
-      <h1>placeholder</h1>
+      {resumes.map((resume) => {
+        return (
+          <div key={resume.id}>
+            <TinderCard>sdfa</TinderCard>
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
 export default RecruiterPage;
