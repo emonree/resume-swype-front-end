61e063a71aaa71acbb531166fcd5c601a81a60af
Emon Li <emonli.328@gmail.com>
2023-01-19 13:50:55 -0500

    added Provider component to index.js for react-wrap-balancer

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import store from "./app/store";
 import { Provider } from "react-redux";
+import { Provider as ReactWrapProvider } from "react-wrap-balancer";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -11,7 +12,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <React.StrictMode>
-      <App />
+      <ReactWrapProvider>
+        <App />
+      </ReactWrapProvider>
     </React.StrictMode>
   </Provider>
 );
